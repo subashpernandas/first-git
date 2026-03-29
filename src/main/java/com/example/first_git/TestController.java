3673f7be48c5a224f5bd814f38c769cdbf6bd2a5
subashpernandas <subashpstalin@gmail.com>
2026-03-29 16:38:19 +0530

    add TestController @GetMapping

--- src/main/java/com/example/first_git/TestController.java
+++ src/main/java/com/example/first_git/TestController.java
@@ -1,4 +1,15 @@
 package com.example.first_git;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/test")
 public class TestController  {
+
+    @GetMapping("/get")
+    public String getCall() {
+        return "GET";
+    }
 }
