aaa46b9bae2541a5836d313e6013413e0f94aed3
subashpernandas <subashpstalin@gmail.com>
2026-03-29 17:02:37 +0530

    add @PostMapping

--- src/main/java/com/example/first_git/TestController.java
+++ src/main/java/com/example/first_git/TestController.java
@@ -1,6 +1,7 @@
 package com.example.first_git;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,6 +14,7 @@ public class TestController  {
         return "SUBASH-PERNANDAS-SAVARI-JESU-VIYAGULA-INNACI-ARULAYAMMAL-MARIANTHONI-KANNAN-MONISH-ARYAN";
     }
 
+    @PostMapping
     public String getCall2() {
         return "THIS FAMILY BELONGS FROM K M KOTTAI AND WELL EDUCTAED";
     }
